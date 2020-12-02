56bb7593b72146207d32c9bbd2789503187f4da2
gbrancaglione <45099000+gbrancaglione@users.noreply.github.com>
2020-12-02 10:11:42 +0100

    Adding i18n in our project: (#6)
    - Better control of messages
    - We still need to add translation for our error messages

--- src/main/java/com/capim/credit/CreditApplication.java
+++ src/main/java/com/capim/credit/CreditApplication.java
@@ -2,12 +2,34 @@ package com.capim.credit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
-public class CreditApplication {
+public class CreditApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         SpringApplication.run(CreditApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        return new CookieLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
 }
