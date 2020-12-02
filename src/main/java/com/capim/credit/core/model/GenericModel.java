a9bf1a63223f60f6da26e3ecbbe555c85bfec1f8
Gauthier Hacout <gauthier.hacout@hei.yncrea.fr>
2020-12-02 18:18:15 +0100

    Addition of logging possibilities

--- src/main/java/com/capim/credit/core/model/GenericModel.java
+++ src/main/java/com/capim/credit/core/model/GenericModel.java
@@ -1,13 +1,15 @@
 package com.capim.credit.core.model;
 
+import lombok.Getter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
 
+@Getter
 @MappedSuperclass
-public class GenericModel {
+public abstract class GenericModel {
 
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
